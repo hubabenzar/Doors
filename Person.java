08ca0a4f6fcb229cb941c6b91ee9cf799ca686f6
Huba Benzar <34026836+hubabenzar@users.noreply.github.com>
2019-02-26 02:30:06 +0000

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,12 +1,5 @@
-/*
-Course:			COMP 122
-Lecturer:		DR Russel Martin
-Title:			Assignment 4 - Person
-Name: 			Huba Ferenc Benzar
-Student ID:		201262833
-Email:			H.Benzar@student.Liverpool.ac.uk
-Deadline: 		16/05/2018 - 1700
-*/
+//Huba Ferenc Benzar
+
 abstract class Person{
     //numbDoors is defined
 	final int numDoors;
